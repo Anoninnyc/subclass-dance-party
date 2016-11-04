f7a6dbec811b45cab7d4aa8ced5dbf2a4e0b627f
Krishan Arya <krishanmarya1@gmail.com>
2016-11-04 20:20:57 +1100

    Lint

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,15 +1,15 @@
 // MAKE THE BELOW PC!!!
 //trump
 var makeTrump = function(top, left, timeBetweenSteps) {
-makeDancer.call(this,top, left, timeBetweenSteps);
-this.top=top;
-this.left=left;
- 
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.top = top;
+  this.left = left;
+
 
 }
 
-makeTrump.prototype=Object.create(makeDancer.prototype);
-makeTrump.prototype.constructor=makeTrump;
+makeTrump.prototype = Object.create(makeDancer.prototype);
+makeTrump.prototype.constructor = makeTrump;
 
 
 
@@ -17,11 +17,11 @@ makeTrump.prototype.constructor=makeTrump;
 
 //Clinton
 var makeClinton = function(top, left, timeBetweenSteps) {
-makeDancer.call(this,top, left, timeBetweenSteps);
-this.$node=$('<span class="dancing clinton"> <img class="clinton" src="/Users/student/Downloads/CLINTON.png"></span>');
-this.setPosition(top, left, timeBetweenSteps)
-this.top=top;
-this.left=left;
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancing clinton"> <img class="clinton" src="/Users/student/Downloads/CLINTON.png"></span>');
+  this.setPosition(top, left, timeBetweenSteps)
+  this.top = top;
+  this.left = left;
 
 
 };
@@ -30,21 +30,27 @@ makeClinton.prototype = Object.create(makeDancer.prototype);
 makeClinton.prototype.constructor = makeClinton;
 
 makeClinton.prototype.step = function(timeBetweenSteps) {
-  this.$node.animate({"top":"-=20px"}, "slow");
-   
-   this.$node.animate({"top":"+=20px"}, "slow");
+  this.$node.animate({
+    "top": "-=20px"
+  }, "slow");
+
+  this.$node.animate({
+    "top": "+=20px"
+  }, "slow");
   var xxx = this;
-  setTimeout(function(){xxx.step(this.timeBetweenSteps)},this.timeBetweenSteps);
-  };
+  setTimeout(function() {
+    xxx.step(this.timeBetweenSteps)
+  }, this.timeBetweenSteps);
+};
 
 
 
 var makeSanders = function(top, left, timeBetweenSteps) {
-makeDancer.call(this,top, left, timeBetweenSteps);
-this.$node=$('<span class="dancing sanders"><img class="sandwich" src="/Users/student/Downloads/sanders.png"></span>');
-this.setPosition(top, left, timeBetweenSteps);
-this.top=top;
-this.left=left;
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="dancing sanders"><img class="sandwich" src="/Users/student/Downloads/sanders.png"></span>');
+  this.setPosition(top, left, timeBetweenSteps);
+  this.top = top;
+  this.left = left;
 
 
 };
@@ -59,14 +65,25 @@ makeSanders.prototype.constructor = makeSanders;
 
 makeSanders.prototype.step = function(timeBetweenSteps) {
 
-   this.$node.animate({"top":"-=25px", "left":"+=25px"}, "slow");
-   
-    this.$node.animate({"top":"+=25px", "left":"-=25px"}, "slow");
-     this.$node.animate({"top":"-=25px", "left":"-=25px"}, "slow");
-      this.$node.animate({"top":"+=25px", "left":"+=25px"}, "slow");
+  this.$node.animate({
+    "top": "-=25px",
+    "left": "+=25px"
+  }, "slow");
+
+  this.$node.animate({
+    "top": "+=25px",
+    "left": "-=25px"
+  }, "slow");
+  this.$node.animate({
+    "top": "-=25px",
+    "left": "-=25px"
+  }, "slow");
+  this.$node.animate({
+    "top": "+=25px",
+    "left": "+=25px"
+  }, "slow");
   var xxx = this;
-  setTimeout(function(){xxx.step(this.timeBetweenSteps)},this.timeBetweenSteps)
-  };
-
-
-
+  setTimeout(function() {
+    xxx.step(this.timeBetweenSteps)
+  }, this.timeBetweenSteps)
+};
